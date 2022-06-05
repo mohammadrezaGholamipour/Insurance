8b2c72322c872a316cc5c5d5a418eacac17f1c0f
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2022-06-05 17:25:14 +0430

    Fix

--- src/components/Main/sections/InsuranceList.vue
+++ src/components/Main/sections/InsuranceList.vue
@@ -6,7 +6,7 @@
       v-for="items in InsurancesList"
       :key="items"
     >
-      <img :src="items" width="100" height="100" alt="بیمه" />
+      <img :src="items" width="80" height="80" alt="بیمه" />
       
     </div>
   </div>
